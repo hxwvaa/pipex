5da15d05db839fcb2ffe3c038210377ddfcde2ff
hxwvaa <hamdhanb8@gmail.com>
2024-06-03 00:08:32 +0400

    pipex mandatory done

--- pipex.c
+++ pipex.c
@@ -13,7 +13,7 @@ void	execute(char *cmd, char **env)
 		ft_putendl_fd(f_cmds[0], 2);
 		free_arr(f_cmds);
 		free(path);
-		exit(2);
+		exit(1);
 	}
 }
 
@@ -23,7 +23,7 @@ void	child(char **av, char **env, int *fd)
 
 	f = open(av[1], O_RDONLY);
 	if (f == -1)
-		exit(-1);
+		exit(1);
 	close(fd[0]);
 	dup2(f, 0);
 	dup2(fd[1], 1);
@@ -36,7 +36,7 @@ void	parent(char **av, char **env, int *fd)
 
 	f = open(av[4], O_WRONLY | O_CREAT, 0777);
 	if (f == -1)
-		exit(-1);
+		exit(1);
 	close(fd[1]);
 	dup2(f, 1);
 	dup2(fd[0], 0);
@@ -52,13 +52,13 @@ int	main(int ac, char **av, char **env)
 	{
 		ft_putstr_fd("WRONG FORMAT!\n", 2);
 		ft_putendl_fd("CORRECT FORMAT: ./pipex file1 cmd1 cmd2 file2", 2);
-		exit(5);
+		exit(1);
 	}
 	if (pipe(fd) == -1)
-		exit(-1);
+		exit(1);
 	id = fork();
 	if (id == -1)
-		exit(-1);
+		exit(1);
 	if (id == 0)
 		child(av, env, fd);
 	parent(av, env, fd);
