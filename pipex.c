ecb662fec0f6d6bb75934f560ab7421dc5b51a17
hxwvaa <hamdhanb8@gmail.com>
2024-06-02 14:20:59 +0400

    pipex

--- pipex.c
+++ pipex.c
@@ -1,6 +1,30 @@
 #include "pipex.h"
 
-int	main(int ac, char **av)
+void child(char **av, char **env, int *fd)
+{
+	int f;
+	f = open(av[1], O_RDONLY);
+	if (f == -1)
+		exit(-1);
+	close(fd[0]);
+	dup2(f, 0);
+	dup2(fd[1], 1);
+	execute(av[2], env);
+}
+
+void parent(char **av, char **env, int *fd)
+{
+	int f;
+	f = open(av[4], O_WRONLY | O_CREAT, 0777);
+	if (f == -1)
+		exit(-1);
+	close(fd[1]);
+	dup2(f, 1);
+	dup2(fd[0], 0);
+	execute(av[3], env);
+}
+
+int	main(int ac, char **av, char **env)
 {
 	int		fd[2];
 	pid_t	id;
@@ -10,5 +34,7 @@ int	main(int ac, char **av)
 	id = fork();
 	if (id == -1)
 		exit(-1);
-	open();
+	if (id == 0)
+		child(av, env, fd);
+	parent(av, env, fd);
 }
